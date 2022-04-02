8e4648421336de72ee5feaf7363b60d604d446ef
Nmalsha <malshis@yahoo.com>
2022-04-02 21:47:41 +0200

    creating tick table in the database using doctrine

---
+++ migrations/Version20220402194206.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220402194206 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE trick (id INT AUTO_INCREMENT NOT NULL, user_name VARCHAR(70) NOT NULL, trick_name VARCHAR(70) NOT NULL, description VARCHAR(255) NOT NULL, photos VARCHAR(255) DEFAULT NULL, video VARCHAR(255) DEFAULT NULL, categorie VARCHAR(100) NOT NULL, created_on DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE trick');
+    }
+}
